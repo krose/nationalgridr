fb38dcd4d33f833fd0c489ec74ea6c3b0a7db0c6
Kenneth Rose <kennethrose82@gmail.com>
2022-01-12 14:13:27 +0100

    data dictionary added.

--- R/ng_data_item_explorer.R
+++ R/ng_data_item_explorer.R
@@ -7,3 +7,19 @@
 ukgasapi::dataItemExplorer
 
 
+
+#' Get the data dictionary for the data item explorer.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' ng_data_item_explorer_dictionary()
+#'
+#'
+ng_data_item_explorer_dictionary <- function(){
+
+  readr::read_csv2(system.file("data-dictionary/API_List_v2.0_0.csv", package = "nationalgridr"), col_types = "cccccccc")
+}
+
+
